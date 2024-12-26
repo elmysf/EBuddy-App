f4877de72bf6dff64c6ea73d04427eefad51cdb2
elmysf <elmysf@yahoo.com.sg>
2024-12-26 21:13:41 +0700

    Initial Commit

---
+++ EBuddy App/EBuddy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddy_AppApp.swift
+//  EBuddy App
+//
+//  Created by Elmo on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EBuddy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EBuddy AppTests/EBuddy_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddy_AppTests.swift
+//  EBuddy AppTests
+//
+//  Created by Elmo on 26/12/24.
+//
+
+import Testing
+@testable import EBuddy_App
+
+struct EBuddy_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
